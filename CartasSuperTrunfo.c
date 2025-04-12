21c4baf1556fe858c7feb7d064e17d0212febb78
gilson0741 <63420007+gilson0741@users.noreply.github.com>
2025-04-12 21:10:07 +0000

    SUPERTRUNFO AVENTUREIRO DENSIDADE E PERCAPITA

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,8 @@ int main() {
     float area1, area2;
     float PIB1, PIB2;
     int pontoTuristico1, pontoTuristico2;
+    float Denspop1,Denspop2;
+    float PIBpcap1,PIBpcap2;
 
   // INSERINDO INFORMAÇÕES DA CARTA 1
     printf("Digite os dados da Carta 1:\n");
@@ -31,6 +33,8 @@ int main() {
     scanf(" %f", &PIB1);
     printf("Numero de Pontos Turisticos: \n");
     scanf(" %d", &pontoTuristico1);
+    PIBpcap1 = PIB1/area1;
+    Denspop1 = populacao1/area1;
 
   // INSERINDO INFORMAÇÕES DA CARTA 2
     printf("\nDigite os dados da Carta 2:\n");
@@ -48,9 +52,12 @@ int main() {
     printf("PIB (em bilhoes de reais): \n");
     scanf(" %f", &PIB2);
     printf("Numero de Pontos Turisticos: \n");
-    scanf(" %d", &pontoTuristico2);  
+    scanf(" %d", &pontoTuristico2);
+    PIBpcap2 = PIB2/area2;
+    Denspop2 = populacao2/area2;  
   
      //IMPRIMINDO INFORMAÇOES DA CARTA 1
+     
      printf("\nCarta 1:\n");
      printf("Estado: %c\n", estado1);
      printf("Codigo: %c%s\n",estado1,codigo1);
@@ -59,7 +66,11 @@ int main() {
      printf("Area: %.2f km2\n", area1);
      printf("PIB: %.2f bilhOes de reais\n", PIB1);
      printf("Numero de Pontos Turisticos: %d\n", pontoTuristico1);
+     printf("Densidade populacional: %.2f hab/km2\n",Denspop1);
+     printf("PIB per Capita: %.2f reais",PIBpcap1);
+    
      //IMPRIMINDO INFORMAÇOES DA CARTA 2
+    
      printf("\nCarta 2:\n");
      printf("Estado: %c\n", estado2);
      printf("Codigo: %s\n", codigo2);
@@ -68,5 +79,7 @@ int main() {
      printf("Area: %.2f km2\n", area2);
      printf("PIB: %.2f bilhoes de reais\n", PIB2);
      printf("Numero de Pontos Turisticos: %d\n", pontoTuristico2);
+     printf("Densidade populacional: %.2f hab/km2\n",Denspop2);
+     printf("PIB per Capita: %.2f reais",PIBpcap2);
     return 0;
 }
